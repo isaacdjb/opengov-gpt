2f93a724cbecbab154288e9928c0ca4bc631fd95
Isaac <isaacdjbcc@gmail.com>
2025-09-18 23:20:27 +0100

    Initialise vite-react template

---
+++ politimap/.storybook/preview.ts
@@ -0,0 +1,43 @@
+import type { Preview } from '@storybook/react-vite'
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from '../src/components/theme';
+
+import { createGlobalStyle } from 'styled-components';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+  `;
+
+const preview: Preview = {
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+
+        a11y: {
+            // 'todo' - show a11y violations in the test UI only
+            // 'error' - fail CI on a11y violations
+            // 'off' - skip a11y checks entirely
+            test: 'todo'
+        }
+    },
+};
+
+export const decorators = [
+    withThemeFromJSXProvider({
+        themes: {
+            light: lightTheme,
+            dark: darkTheme,
+        },
+        defaultTheme: 'light',
+        Provider: ThemeProvider,
+        GlobalStyles,
+    })];
+
+export default preview;
